f45b8c44f3e34dc3e5c68a8cf0b4581467531ca4
yangkun <1149642605@qq.com>
2019-04-14 23:53:21 +0800

    2019/4/14 23:53

--- registered/registered.js
+++ registered/registered.js
@@ -3,6 +3,9 @@ var checkAccount = true;
 var checkUsername = true;
 var checkPassword = true;
 var checkPhone = true;
+var checkVerificationCode = true;
+var waitTime = 60;
+var verificationCode = '';
 
 function registeredAccount(){
     var account = $('#account').val();
@@ -49,51 +52,133 @@ function registeredAccount(){
 
 }
 
-function testInput(obj){
-    //console.log(obj)
-    var placeholder = $(obj).prop('placeholder');
-    var value = $(obj).val();
-    placeholder = placeholder.split('不')[0];
-    if(value === ''){
-        $(obj).prop('placeholder',placeholder + '不能为空');
-        $(obj).css('border-color','#FF0000');
-    }else{
-        $(obj).css('border-color','');
+
+function getVerificationCode(){
+    var phone = $('#phone').val();
+    //验证手机号码格式
+    if(phone.length !== 11 || !phone.startsWith('1')){
+        alert('手机号码不合法');
+        return;
     }
-
-    checkAccount = true;
-    checkUsername = true;
-    checkPassword = true;
-    checkPhone = true;
-
-    //console.log(placeholder);
-    if(placeholder === '账号'){
-        //判断账号是否已存在
-        $.ajax({
-            method:'get',
-            url:'http://127.0.0.1:8080/checkAccountExist',
-            data:{
-                account:value
-            }
-        }).done(function(msg){
-            // console.log(msg);
-            if(msg.isExist === 1){
-                alert('该账号已存在');
-                checkAccount = false;
-                $(obj).val('');
-            }
-        });
-
-    }else if(placeholder === '用户名'){
-
-    }else if(placeholder === '密码'){
-
-    }else if(placeholder === '手机号码'){
-        if(value.length !== 11 || !value.startsWith('1')){
-            //alert('手机号码不合法');
-            checkPhone = false;
+    $('#verificationBtn').attr('disabled',true);
+    var timerId = setInterval(function(){
+        $('#verificationBtn').text(waitTime+'秒后重试');
+        //console.log(waitTime);
+        waitTime--;
+        if(waitTime === 0){
+            clearInterval(timerId);
+            $('#verificationBtn').text('获取验证码');
+            $('#verificationBtn').attr('disabled',false);
+            waitTime = 60;
         }
-        
-    }
+    },1000);
+    //调用服务端的短信服务
+    $.ajax({
+        method:'get',
+        url:'http://127.0.0.1:8080/sendSms',
+        data:{
+            phone:phone
+        }
+    }).done(function(msg){
+        console.log(msg);
+        verificationCode = msg.verificationCode;
+    })
+}
 
+//检测账号
+function checkAccountInput(obj){
+    checkAccount = true;
+    var data = $(obj).val();
+    //判断账号是否为空
+    if(data === ''){
+        alert('账号不能为空');
+        checkAccount = false;
+        $(obj).css('border-color','#FF0000');
+        return;
+    }
+    //判断账号是否已存在
+    $.ajax({
+        method:'get',
+        url:'http://127.0.0.1:8080/checkAccountExist',
+        data:{
+            account:data
+        }
+    }).done(function(msg){
+        // console.log(msg);
+        if(msg.isExist === 1){
+            alert('该账号已存在');
+            checkAccount = false;
+            $(obj).css('border-color','#FF0000');
+            $(obj).val('');
+        }else{
+            $(obj).css('border-color','#07DC00');
+        }
+    });
+}
+
+//检测用户名
+function checkUsernameInput(obj){
+    checkUsername = true;
+    var data = $(obj).val();
+    if(data === ''){
+        alert('用户名不能为空');
+        $(obj).css('border-color','#FF0000');
+        checkUsername = false;
+        return;
+    }
+    $(obj).css('border-color','#07DC00');
+}
+
+//检测密码
+function checkPasswordInput(obj){
+    checkPassword = true;
+    var data = $(obj).val();
+    if(data === ''){
+        alert('密码不能为空');
+        $(obj).css('border-color','#FF0000');
+        checkPassword = false;
+        return;
+    }else if(data.length < 6){
+        alert('密码不能少于6位');
+        $(obj).css('border-color','#FF0000');
+        checkPassword = false;
+        return;
+    }
+    $(obj).css('border-color','#07DC00');
+}
+
+//检测手机号码
+function checkPhoneInput(obj){
+    checkPhone = true;
+    var data = $(obj).val();
+    if(data === ''){
+        alert('手机号码不能为空');
+        $(obj).css('border-color','#FF0000');
+        checkPhone = false;
+        return;
+    }else if(data.length !== 11 || !data.startsWith('1')){
+        alert('手机号码不合法');
+        $(obj).css('border-color','#FF0000');
+        checkPhone = false;
+        return;
+    }
+    $(obj).css('border-color','#07DC00');
+}
+
+//检测验证码
+function checkVerificationCodeInput(obj){
+    checkVerificationCode = true;
+    var data = $(obj).val();
+    if(data === ''){
+        alert('验证不能为空');
+        $(obj).css('border-color','#FF0000');
+        checkVerificationCode = false;
+        return;
+    }else if(data !== verificationCode){
+        alert('验证码不正确');
+        $(obj).css('border-color','#FF0000');
+        checkVerificationCode = false;
+        return;
+     }
+    $(obj).css('border-color','#07DC00');
 }
