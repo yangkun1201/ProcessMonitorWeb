ccafec1d8cae63226a8b4639afcc2ef17a5a260d
yangkun <1149642605@qq.com>
2019-04-15 22:23:38 +0800

    2019/4/15 22:23

--- login/login.js
+++ login/login.js
@@ -37,3 +37,56 @@ function registeredAccount(){
     //console.log(window.location);
     window.location.href = 'file:///G:/%E6%96%87%E6%A1%A3/%E6%AF%95%E4%B8%9A%E8%AE%BE%E8%AE%A1/ProcessMonitorWeb/registered/registered.html';
 }
+
+//改变登陆方式
+function changeLoginMethod(obj){
+    var method = $(obj).text();
+    if(method === '二维码登陆'){
+        $('#loginByAccount').css('display','none');
+        $('#loginByQrcode').css('display','block');
+        $(obj).text('账号密码登陆');
+        //获取二维码图片
+        getQrCodeFromServer();
+
+
+    }else if(method === '账号密码登陆'){
+        $('#loginByQrcode').css('display','none');
+        $('#loginByAccount').css('display','block');
+        $(obj).text('二维码登陆');
+    }
+    //console.log(method);
+}
+
+//获取二维码图片
+function getQrCodeFromServer(){
+    $.ajax({
+        method:'get',
+        url:'http://127.0.0.1:8080/getQrCodeImageBase64'
+    }).done(function(msg){
+        console.log(msg.uuid);
+        imgUrl = 'data:image/jpg;base64,'+msg.qrCodeInBase64;
+        $('#qrCodeImg').prop('src',imgUrl);
+        pollingQrCodeStatus(msg.uuid);
+    })
+}
+
+//轮询二维码扫描状态
+function pollingQrCodeStatus(uuid){
+    var timeId = setInterval(function(){
+        console.log('polling');
+        $.ajax({
+            method:'get',
+            url:'http://127.0.0.1:8080/queryQrCodeScanStatus',
+            data:{
+                uuid:uuid
+            }
+        }).done(function(msg){
+            if(msg.scanStatus === 0){
+                var account = msg.account;
+                clearInterval(timeId);
+                console.log('polling success : ' + account);
+                window.location.href = 'file:///G:/%E6%96%87%E6%A1%A3/%E6%AF%95%E4%B8%9A%E8%AE%BE%E8%AE%A1/ProcessMonitorWeb/index.html?account=' + account;
+            }
+        })
+    },200);
+}
